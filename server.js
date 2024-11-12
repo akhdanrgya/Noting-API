f4e18c5f13a4b58bd39fd79e23d4fd906a3012e4
akhdanrgya <akhdan.anargya@gmail.com>
2024-11-12 23:33:38 +0700

    Ready to dev

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+
+const app = express()
+const port = 5000
+
+app.get("/api", (req,res) => {
+    res.json("Test")
+})
+
+app.get("/", (req, res)=> {
+    res.json("Selamat datang di API NOTING ❤️😍")
+})
+
+app.listen(port, (req, res) => {
+    console.log(`listening server on port http://localhost:${port}`)
+})
