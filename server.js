5349b23a16530e46eeced4d98564228c030b744d
akhdanrgya <akhdan.anargya@gmail.com>
2024-11-17 17:54:40 +0700

    jadinya pake mongodb

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
-const express = require("express")
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const app = express()
-const port = 5000
 
-app.get("/", (req, res)=> {
-    res.json("Selamat datang di API NOTING ❤️😍")
-})
+app.use(express.json())
 
-app.listen(port, (req, res) => {
-    console.log(`listening server on port http://localhost:${port}`)
-})
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('MongoDB connected ✅'))
+  .catch(err => console.error('Error connecting to MongoDB:', err))
+
+// listen ke port yang diambil dari .env
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
